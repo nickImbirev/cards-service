522f837166fd7cc71ea47d11d93657c5e3c9da61
Nikolai Imbirev <nikolay.imbirev@broadcom.com>
2022-08-03 17:51:38 +0200

    document the endpoint with swagger

--- src/main/java/org/cards_tracker/Main.java
+++ src/main/java/org/cards_tracker/Main.java
@@ -2,6 +2,10 @@ package org.cards_tracker;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.javalin.Javalin;
+import io.javalin.plugin.openapi.OpenApiOptions;
+import io.javalin.plugin.openapi.OpenApiPlugin;
+import io.javalin.plugin.openapi.ui.SwaggerOptions;
+import io.swagger.v3.oas.models.info.Info;
 import org.cards_tracker.controller.CardController;
 import org.cards_tracker.controller.error.EndpointRegistrationException;
 import org.cards_tracker.service.CardService;
@@ -16,7 +20,14 @@ public class Main {
     private static final Logger log = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
-        final Javalin app = Javalin.create().start(8081);
+        final Javalin app = Javalin.create(config -> config.registerPlugin(
+                new OpenApiPlugin(
+                        new OpenApiOptions
+                                (new Info().title("Cards service").version("1.0.0").description("Cards service"))
+                                .swagger(new SwaggerOptions("/swagger-ui").title("Card service API Documentation"))
+                                .path("/swagger-docs")
+                ))).start(8081);
+
         final ObjectMapper objectMapper = new ObjectMapper();
         CardService cardService =
                 new ScheduledInMemoryCardService(TimeUnit.MINUTES, 1L, 5);
--- src/main/java/org/cards_tracker/controller/CardController.java
+++ src/main/java/org/cards_tracker/controller/CardController.java
@@ -3,6 +3,8 @@ package org.cards_tracker.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.javalin.Javalin;
 import io.javalin.http.HttpCode;
+import io.javalin.plugin.openapi.dsl.OpenApiBuilder;
+import io.javalin.plugin.openapi.dsl.OpenApiDocumentation;
 import org.cards_tracker.controller.dto.Card;
 import org.cards_tracker.controller.dto.ErrorDto;
 import org.cards_tracker.controller.error.EndpointRegistrationException;
@@ -16,9 +18,17 @@ public class CardController {
     public static void registerCreateCardEndpoint(@NotNull final Javalin app,
                                                   @NotNull final ObjectMapper objectMapper,
                                                   @NotNull final CardService cardService) throws EndpointRegistrationException {
+        final OpenApiDocumentation apiDocumentation = OpenApiBuilder
+                .document()
+                .operation(operation -> {
+                    operation.description("Create brand new card (task).");
+                })
+                .body(Card.class)
+                .json(String.valueOf(HttpCode.BAD_REQUEST.getStatus()), ErrorDto.class)
+                .result(String.valueOf(HttpCode.CREATED.getStatus()));
         final String path = "/card";
         try {
-            app.post(path, ctx -> {
+            app.post(path, OpenApiBuilder.documented(apiDocumentation, ctx -> {
                 final Card cardToCreate;
                 try {
                     cardToCreate = ctx.bodyAsClass(Card.class);
@@ -37,7 +47,7 @@ public class CardController {
                     return;
                 }
                 ctx.status(HttpCode.CREATED);
-            });
+            }));
         } catch (Exception e) {
             throw new EndpointRegistrationException(path, HttpMethod.POST, e);
         }
