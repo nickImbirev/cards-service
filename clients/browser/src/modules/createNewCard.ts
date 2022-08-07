7df48ac2514ff29a9a71eac98e2093e266a36548
Daria Imbireva <imbireva.daria@gmail.com>
2022-08-07 17:17:59 +0300

    add creating new card feature

---
+++ clients/browser/src/modules/createNewCard.ts
@@ -0,0 +1,20 @@
+import { sendRequest } from '../helpers/sendRequest';
+
+const newCardInput = document.querySelector('.new-card__input') as HTMLInputElement;
+const newCardButton = document.querySelector('.new-card__button') as HTMLButtonElement;
+const newCardURL = 'http://localhost:8081/card';
+
+const createNewCard = () => {
+  const title = newCardInput.value;
+  sendRequest(newCardURL, 'POST', {title: title})
+    .then(response => {
+      if (response.status === 201) {
+        alert('New card was created!');
+      } else {
+        alert('Error! New card was not created...');
+      }
+    })
+    .catch(() => alert('Error! New card was not created...'));
+};
+
+newCardButton.addEventListener('click', createNewCard);
