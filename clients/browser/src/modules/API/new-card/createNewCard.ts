8ba73e4f5c20f5431b748f319dff59780e6fe752
Daria Imbireva <imbireva.daria@gmail.com>
2022-08-20 20:03:56 +0300

    rename variables to avoid naming based on the type

--- clients/browser/src/modules/API/new-card/createNewCard.ts
+++ clients/browser/src/modules/API/new-card/createNewCard.ts
@@ -2,6 +2,6 @@ import { sendRequest } from '../helpers/sendRequest';
 
 const newCardURL = 'http://localhost:8081/card';
 
-export const createNewCard = (userTitleStr: string): Promise<Response> => {
-  return sendRequest(newCardURL, 'POST', { title: userTitleStr });
+export const createNewCard = (userTitle: string): Promise<Response> => {
+  return sendRequest(newCardURL, 'POST', { title: userTitle });
 };
--- clients/browser/src/modules/user-interaction/today-cards/showTodayCards.ts
+++ clients/browser/src/modules/user-interaction/today-cards/showTodayCards.ts
@@ -3,7 +3,7 @@ import { collectCardContent } from '../helpers/collectCardContent';
 const todayCardsContainer = document.querySelector('.today-cards') as HTMLDivElement;
 
 export const showTodayCards = (todayCardsList: [string]): void => {
-  todayCardsList.forEach(todayCardStr => {
-    todayCardsContainer.append(collectCardContent(todayCardStr));
+  todayCardsList.forEach(todayCard => {
+    todayCardsContainer.append(collectCardContent(todayCard));
   });
 };
