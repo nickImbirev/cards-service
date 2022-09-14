8833856aa22981e687e1e07a1a642e3841a90708
Daria Imbireva <imbireva.daria@gmail.com>
2022-09-14 13:36:41 +0300

    update file to fix linter errors

--- clients/browser/src/modules/helpers/createNode.ts
+++ clients/browser/src/modules/helpers/createNode.ts
@@ -1,19 +1,12 @@
 export const createNode = (
   tagName: string,
   className: string,
-  textContent?: string,
-  attributesNames?: [string],
-  attributesValues?: [string]
+  textContent?: string
 ): HTMLElement => {
   const node: HTMLElement = document.createElement(tagName);
   node.className = className;
   if (textContent) {
     node.textContent = textContent;
-  }
-  if (attributesNames && attributesValues) {
-    attributesNames.forEach((attributeName, index) => {
-      node.setAttribute(attributeName, attributesValues[index]);
-    });
   }
   return node;
 };
