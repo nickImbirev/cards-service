08cb5d03fc8b2c449dc6569eedb18b530448fc7e
Nikolai Imbirev <nikolay.imbirev@broadcom.com>
2022-12-02 14:04:30 +0100

    remove unused constructor

--- server/src/main/java/org/cards_tracker/error/IncorrectCardPriorityScheduleException.java
+++ server/src/main/java/org/cards_tracker/error/IncorrectCardPriorityScheduleException.java
@@ -1,13 +1,8 @@
 package org.cards_tracker.error;
 
-import org.cards_tracker.domain.CardPriorityUpdateSchedule;
 import org.jetbrains.annotations.NotNull;
 
 public class IncorrectCardPriorityScheduleException extends Exception {
-    public IncorrectCardPriorityScheduleException(@NotNull final CardPriorityUpdateSchedule priorityUpdateSchedule) {
-        super("The card priority update schedule: " + priorityUpdateSchedule + " is incorrect");
-    }
-
     public IncorrectCardPriorityScheduleException(@NotNull final String reason) {
         super(reason);
     }
